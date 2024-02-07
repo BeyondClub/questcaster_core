3760ae19d8cd7cafa93e6065779f78660f26ba47
Ehtesham <siddiquie.ehtesham@gmail.com>
2024-02-07 09:35:14 +0530

    flex error fixes

--- app/api/images/start/route.tsx
+++ app/api/images/start/route.tsx
@@ -1,10 +1,27 @@
 import { ImageResponse } from '@vercel/og';
 import { NextRequest } from 'next/server';
 
+export const config = {
+  runtime: 'edge',
+};
+
 export async function GET(request: NextRequest) {
   return new ImageResponse(
     (
-      <div className='p-10 flex'>
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#fff',
+          fontSize: 32,
+          fontWeight: 600,
+        }}
+        className='p-10 flex'
+      >
         <h1 className='font-black text-xl'>Its working</h1>
       </div>
     ),
