f5e0beaa0e52a57093efce3ce93b16363e4b91a4
Ehtesham <siddiquie.ehtesham@gmail.com>
2024-02-07 09:30:46 +0530

    image change

--- app/api/images/start/route.tsx
+++ app/api/images/start/route.tsx
@@ -1,70 +1,70 @@
-// import { ImageResponse } from '@vercel/og';
-// import { NextRequest } from 'next/server';
+import { ImageResponse } from '@vercel/og';
+import { NextRequest } from 'next/server';
 
-// export async function GET(request: NextRequest) {
-//   return new ImageResponse(
-//     (
-//       <div className='p-10'>
-//         <h1 className='font-black text-xl'>Its working</h1>
-//       </div>
-//     ),
-//     {
-//       width: 1200,
-//       height: 630,
-//     }
-//   );
-// }
-
-import { NextResponse } from 'next/server';
-import satori from 'satori';
-import sharp from 'sharp';
-import { join } from 'path';
-import * as fs from 'fs';
-
-export async function GET() {
-  const svg = await satori(
-    <div
-      style={{
-        justifyContent: 'center',
-        alignItems: 'center',
-        display: 'flex',
-        flexDirection: 'column',
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'white',
-        padding: 50,
-        lineHeight: 1.2,
-        fontSize: 24,
-        color: 'black',
-      }}
-    >
-      <h1>beyondClub!</h1>
-      <div style={{ display: 'flex' }}>
-        Click to yoink the flag{' '}
-        <img
-          width='32'
-          height='32'
-          alt='weweew'
-          src='https://cdnjs.cloudflare.com/ajax/libs/twemoji/15.0.3/72x72/1f6a9.png'
-        />
+export async function GET(request: NextRequest) {
+  return new ImageResponse(
+    (
+      <div className='p-10'>
+        <h1 className='font-black text-xl'>Its working</h1>
       </div>
-    </div>,
+    ),
     {
-      width: 600,
-      height: 400,
-      fonts: [],
+      width: 1200,
+      height: 630,
     }
   );
-
-  const img = await sharp(Buffer.from(svg))
-    .resize(1200)
-    .toFormat('png')
-    .toBuffer();
-  return new NextResponse(img, {
-    status: 200,
-    headers: {
-      'Content-Type': 'image/png',
-      'Cache-Control': 'max-age=10',
-    },
-  });
 }
+
+// import { NextResponse } from 'next/server';
+// import satori from 'satori';
+// import sharp from 'sharp';
+// import { join } from 'path';
+// import * as fs from 'fs';
+
+// export async function GET() {
+//   const svg = await satori(
+//     <div
+//       style={{
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         display: 'flex',
+//         flexDirection: 'column',
+//         width: '100%',
+//         height: '100%',
+//         backgroundColor: 'white',
+//         padding: 50,
+//         lineHeight: 1.2,
+//         fontSize: 24,
+//         color: 'black',
+//       }}
+//     >
+//       <h1>beyondClub!</h1>
+//       <div style={{ display: 'flex' }}>
+//         Click to yoink the flag{' '}
+//         <img
+//           width='32'
+//           height='32'
+//           alt='weweew'
+//           src='https://cdnjs.cloudflare.com/ajax/libs/twemoji/15.0.3/72x72/1f6a9.png'
+//         />
+//       </div>
+//     </div>,
+//     {
+//       width: 600,
+//       height: 400,
+//       fonts: [],
+//     }
+//   );
+
+//   const img = await sharp(Buffer.from(svg))
+//     .resize(1200)
+//     .toFormat('png')
+//     .toBuffer();
+//   return new NextResponse(img, {
+//     status: 200,
+//     headers: {
+//       'Content-Type': 'image/png',
+//       'Cache-Control': 'max-age=10',
+//     },
+//   });
+// }
