b575516138bdc4b0ce3e5d7f2def5ab9651adfcf
Ehtesham <siddiquie.ehtesham@gmail.com>
2024-02-19 19:50:22 +0530

    fix: id

--- app/api/dba/route.tsx
+++ app/api/dba/route.tsx
@@ -3,6 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
   const {
+    id,
     username,
     image_url,
     token_address,
@@ -14,7 +15,7 @@ export async function POST(request: Request) {
   } = await request.json();
 
   try {
-    await sql`INSERT INTO Quests (username, image_url, token_address, verify_follow, verify_recast, verify_tokens, contract_address, token_name) VALUES (${username}, ${image_url}, ${token_address}, ${verify_follow}, ${verify_recast}, ${verify_tokens}, ${contract_address}, ${token_name});`;
+    await sql`INSERT INTO Quests (id, username, image_url, token_address, verify_follow, verify_recast, verify_tokens, contract_address, token_name) VALUES (${id}, ${username}, ${image_url}, ${token_address}, ${verify_follow}, ${verify_recast}, ${verify_tokens}, ${contract_address}, ${token_name});`;
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
