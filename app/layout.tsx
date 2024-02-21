7c75a265c337d7873cfcc5b7d99d265951822314
PJijin <pjijin1@gmail.com>
2024-02-21 09:57:41 +0530

    fix: updated og tags on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,8 @@
 import { MantineProvider } from "@mantine/core";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "QuestCaster",
-  icons: "/favicon.png",
-  description:
-    "No-code NFT Loyalty Platform for brands, creators and communities. Starting at @ETHGlobal",
-};
 
 export default function RootLayout({
   children,
